1fbfe7817117cdd4adde7392a0b0123f6690890f
sarhov <sarhov@sarhov.com>
2017-06-21 18:25:27 +0400

    added slick slider

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,21 @@
+---
+---
+
+
 jQuery(document).ready(function($) {
-  
+	var $slider = $('#jsSlider')
+	.on('init', function(slick) {
+		console.log('fired!');
+		$('#jsSlider').fadeIn(3000);
+	})
+	.slick({
+		fade: true,
+		focusOnSelect: true,
+		lazyLoad: 'ondemand',
+		speed: 1000,
+		nav: false,
+		mobileFirst: true,
+		slidesToShow: 1,
+		dots: true
+	});
 });
